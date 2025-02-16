8ab2cc027cc022e09eacd152c641ce69e1981fd3
왕규호 <wgh8020@nate.com>
2025-02-16 22:22:40 +0900

    [GLA 연간상품추가] 연간상품목록 추가 테스트2

---
+++ Resources/lang/en/yearlyproduct.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'title' => [
+        'yearlyproduct' => 'YearlyProduct',
+        'create yearlyproduct' => 'Create a yearlyproduct',
+        'edit yearlyproduct' => 'Edit a yearlyproduct',
+    ],
+    'button' => [
+        'create yearlyproduct' => 'Create a YearlyProduct',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'weight' => 'Weight',
+    'height' => 'Height',
+    'width' => 'Width',
+    'length' => 'Length'
+];
---
+++ Resources/lang/ko/yearlyproduct.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => [
+        'yearlyproduct' => '연간상품',
+        'create yearlyproduct' => '연간상품 생성',
+        'edit yearlyproduct' => '연간상품 수정',
+    ],
+    'button' => [
+        'create yearlyproduct' => '연간상품 생성',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'weight' => '무게',
+    'height' => '높이',
+    'width' => '너비',
+    'length' => '길이',
+    'shipping_method' => '배송 방법',
+    'shipping_storage' => '배송출발 장소',
+];
