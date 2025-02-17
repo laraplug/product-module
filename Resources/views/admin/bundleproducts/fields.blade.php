92bbf8e4471401002214e9f2c134e77b03411ba2
왕규호 <wgh8020@nate.com>
2025-02-17 17:01:52 +0900

    번들상품생성테스트10

--- Resources/views/admin/bundleproducts/fields.blade.php
+++ Resources/views/admin/bundleproducts/fields.blade.php
@@ -88,151 +88,151 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js"></script>
     <script src="{{ Module::asset('product:js/ng-components/selectize.directive.js') }}"></script>
-{{--    <script>--}}
-{{--    angular.module('bundle', [--}}
-{{--        'selectize'--}}
-{{--    ])--}}
-{{--    .config(function($interpolateProvider) {--}}
-{{--      $interpolateProvider.startSymbol('{%');--}}
-{{--      $interpolateProvider.endSymbol('%}');--}}
-{{--    })--}}
-{{--    .controller('BundleController', function($scope) {--}}
-{{--        console.log('scope',$scope);--}}
-{{--        $scope.bundleItems = [];--}}
+    <script>
+    angular.module('bundle', [
+        'selectize'
+    ])
+    .config(function($interpolateProvider) {
+      $interpolateProvider.startSymbol('{%');
+      $interpolateProvider.endSymbol('%}');
+    })
+    .controller('BundleController', function($scope) {
+        $scope.bundleItems = [];
 
-{{--        $scope.addBundleItem = function(item) {--}}
-{{--            if(!item['product']) return;--}}
-{{--            if(!$scope.bundleItems) $scope.bundleItems = [];--}}
+        $scope.addBundleItem = function(item) {
+            if(!item['product']) return;
+            console.log(item);
+            if(!$scope.bundleItems) $scope.bundleItems = [];
 
-{{--            // 옵션은 존재하면 enabled처리--}}
-{{--            item.product.options.map(function(option) {--}}
-{{--                if(item.option_values[option.slug] !== undefined) {--}}
-{{--                    option.value = item.option_values[option.slug];--}}
-{{--                    option.enabled = 1;--}}
-{{--                }--}}
-{{--                return option;--}}
-{{--            });--}}
+            // 옵션은 존재하면 enabled처리
+            item.product.options.map(function(option) {
+                if(item.option_values[option.slug] !== undefined) {
+                    option.value = item.option_values[option.slug];
+                    option.enabled = 1;
+                }
+                return option;
+            });
 
-{{--            item.product.options.map(function(option) {--}}
-{{--                option.form_field = option.form_field.replace('name="options[', 'name="items[{% $itemIndex %}][options][');--}}
-{{--                option.form_field = option.form_field.replace('name=', 'ng-model="item.option_values[option.slug]" ng-disabled="item.is_readonly || !option.enabled" name=');--}}
-{{--                return option;--}}
-{{--            });--}}
+            item.product.options.map(function(option) {
+                option.form_field = option.form_field.replace('name="options[', 'name="items[{% $itemIndex %}][options][');
+                option.form_field = option.form_field.replace('name=', 'ng-model="item.option_values[option.slug]" ng-disabled="item.is_readonly || !option.enabled" name=');
+                return option;
+            });
 
-{{--            $scope.bundleItems.push(item);--}}
-{{--        };--}}
+            $scope.bundleItems.push(item);
+        };
 
-{{--        $scope.removeBundleItem = function(index) {--}}
-{{--            $scope.bundleItems.splice(index, 1);--}}
-{{--        };--}}
+        $scope.removeBundleItem = function(index) {
+            $scope.bundleItems.splice(index, 1);
+        };
 
-{{--        $scope.calcProductPrice = function (bundleItem) {--}}
-{{--            var total = Number(bundleItem.product.price);--}}
-{{--            var selectedOptions = bundleItem.product.options.filter(function(option) {--}}
-{{--                return option.value !== undefined;--}}
-{{--            });--}}
-{{--            for(var option of selectedOptions) {--}}
-{{--                // 배열타입 옵션이면 가격적용--}}
-{{--                // Apply price if collection type option--}}
-{{--                if(option.is_collection && option.value) {--}}
-{{--                    var filtered = option.values.filter(function( item ) {--}}
-{{--                        return item.code == option.value;--}}
-{{--                    });--}}
-{{--                    if(filtered.length > 0) {--}}
-{{--                      var selected = filtered[0];--}}
-{{--                      if(selected.price_type == 'FIXED') {--}}
-{{--                        total += selected.price_value;--}}
-{{--                      }--}}
-{{--                      else if(selected.price_type == 'PERCENTAGE') {--}}
-{{--                        total += bundleItem.product.sale_price * (selected.price_value / 100);--}}
-{{--                        total = Math.round(total);--}}
-{{--                      }--}}
-{{--                    }--}}
-{{--                }--}}
-{{--            }--}}
-{{--            // 가격이 음수일수는 없음--}}
-{{--            // There's no minus for price--}}
-{{--            if(total < 0) total = 0;--}}
-{{--            return total;--}}
-{{--        };--}}
+        $scope.calcProductPrice = function (bundleItem) {
+            var total = Number(bundleItem.product.price);
+            var selectedOptions = bundleItem.product.options.filter(function(option) {
+                return option.value !== undefined;
+            });
+            for(var option of selectedOptions) {
+                // 배열타입 옵션이면 가격적용
+                // Apply price if collection type option
+                if(option.is_collection && option.value) {
+                    var filtered = option.values.filter(function( item ) {
+                        return item.code == option.value;
+                    });
+                    if(filtered.length > 0) {
+                      var selected = filtered[0];
+                      if(selected.price_type == 'FIXED') {
+                        total += selected.price_value;
+                      }
+                      else if(selected.price_type == 'PERCENTAGE') {
+                        total += bundleItem.product.sale_price * (selected.price_value / 100);
+                        total = Math.round(total);
+                      }
+                    }
+                }
+            }
+            // 가격이 음수일수는 없음
+            // There's no minus for price
+            if(total < 0) total = 0;
+            return total;
+        };
 
-{{--        // Retrieve data from db--}}
-{{--        var savedItems = {!! json_encode( old('items', $product->items) ) !!};--}}
-{{--        savedItems.map(function(item) {--}}
-{{--            $scope.addBundleItem(item);--}}
-{{--        });--}}
+        // Retrieve data from db
+        var savedItems = {!! json_encode( old('items', $product->items) ) !!};
+        savedItems.map(function(item) {
+            $scope.addBundleItem(item);
+        });
 
-{{--        // selectize--}}
-{{--        $scope.selectizeConfig = {--}}
-{{--            valueField: 'id',--}}
-{{--            labelField: 'name',--}}
-{{--            searchField: 'name',--}}
-{{--            create: false,--}}
-{{--            onItemAdd: function(value, $item) {--}}
-{{--                var product = this.options[value];--}}
+        // selectize
+        $scope.selectizeConfig = {
+            valueField: 'id',
+            labelField: 'name',
+            searchField: 'name',
+            create: false,
+            onItemAdd: function(value, $item) {
+                var product = this.options[value];
 
-{{--                $scope.addBundleItem({--}}
-{{--                    'product': product,--}}
-{{--                    'quantity': 1,--}}
-{{--                    'option_values': {}--}}
-{{--                });--}}
+                $scope.addBundleItem({
+                    'product': product,
+                    'quantity': 1,
+                    'option_values': {}
+                });
 
-{{--                this.clear();--}}
-{{--            },--}}
-{{--            render: {--}}
-{{--                option: function(item, escape) {--}}
-{{--                    return '<div>' +--}}
-{{--                        '<span class="title">' +--}}
-{{--                            '<span class="name">' + escape(item.name) + '</span>' +--}}
-{{--                        '</span>' +--}}
-{{--                    '</div>';--}}
-{{--                }--}}
-{{--            },--}}
-{{--            load: function(query, callback) {--}}
-{{--                if (!query.length) return callback();--}}
-{{--                $.ajax({--}}
-{{--                    url: route('api.product.products.indexServerSide') + '?search=' + encodeURIComponent(query),--}}
-{{--                    type: 'GET',--}}
-{{--                    error: function() {--}}
-{{--                        callback();--}}
-{{--                    },--}}
-{{--                    success: function(res) {--}}
-{{--                        callback(res.data);--}}
-{{--                    }--}}
-{{--                });--}}
-{{--            }--}}
-{{--        };--}}
+                this.clear();
+            },
+            render: {
+                option: function(item, escape) {
+                    return '<div>' +
+                        '<span class="title">' +
+                            '<span class="name">' + escape(item.name) + '</span>' +
+                        '</span>' +
+                    '</div>';
+                }
+            },
+            load: function(query, callback) {
+                if (!query.length) return callback();
+                $.ajax({
+                    url: route('api.product.products.indexServerSide') + '?search=' + encodeURIComponent(query),
+                    type: 'GET',
+                    error: function() {
+                        callback();
+                    },
+                    success: function(res) {
+                        callback(res.data);
+                    }
+                });
+            }
+        };
 
-{{--    })--}}
-{{--    .directive('compile', ['$compile', function ($compile) {--}}
-{{--      return function(scope, element, attrs) {--}}
-{{--        scope.$watch(--}}
-{{--          function(scope) {--}}
-{{--            return scope.$eval(attrs.compile);--}}
-{{--          },--}}
-{{--          function(value) {--}}
-{{--            element.html(value);--}}
-{{--            $compile(element.contents())(scope);--}}
-{{--          }--}}
-{{--       )};--}}
-{{--    }])--}}
-{{--    .directive("datepicker", function() {--}}
-{{--        return {--}}
-{{--          restrict: "A",--}}
-{{--          require: "ngModel",--}}
-{{--          link: function(scope, elem, attrs, ngModelCtrl) {--}}
-{{--              elem.on("changeDate", updateModel);--}}
-{{--              elem.datepicker({--}}
-{{--                  language: '{{ locale() }}'--}}
-{{--              });--}}
+    })
+    .directive('compile', ['$compile', function ($compile) {
+      return function(scope, element, attrs) {
+        scope.$watch(
+          function(scope) {
+            return scope.$eval(attrs.compile);
+          },
+          function(value) {
+            element.html(value);
+            $compile(element.contents())(scope);
+          }
+       )};
+    }])
+    .directive("datepicker", function() {
+        return {
+          restrict: "A",
+          require: "ngModel",
+          link: function(scope, elem, attrs, ngModelCtrl) {
+              elem.on("changeDate", updateModel);
+              elem.datepicker({
+                  language: '{{ locale() }}'
+              });
 
-{{--              function updateModel(event) {--}}
-{{--                  ngModelCtrl.$setViewValue(event.date);--}}
-{{--              }--}}
-{{--          }--}}
-{{--        }--}}
-{{--    });--}}
+              function updateModel(event) {
+                  ngModelCtrl.$setViewValue(event.date);
+              }
+          }
+        }
+    });
 
-{{--    angular.bootstrap(document.getElementById("bundle"), ['bundle']);--}}
-{{--    </script>--}}
+    angular.bootstrap(document.getElementById("bundle"), ['bundle']);
+    </script>
 @endpush
